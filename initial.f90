c2e3621b179503b753fa6a60ea7c9fe33abba3e7
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-10-09 11:17:54 +0900

    初期条件変更

--- initial.f90
+++ initial.f90
@@ -10,12 +10,8 @@ subroutine init
     !call glid
     do i = imin-2, imax+2
         do j = jmin-2, jmax+2
-            if (x(i, j) <= 0.0d0) then
                 !-0.5d0 + dx * x(i, j) <= 0.0d0
-                bq(i, j, 1:4) = qtobq(1.0d0, 0.0d0, 0.0d0, 1.0d0)
-            else
-                bq(i, j, 1:4) = qtobq(0.125d0, 0.0d0, 0.0d0, 0.1d0)
-            endif
+                bq(i, j, 1:4) = qtobq(1.0d0, 2.0d0, 0.0d0, 1.0d0/gamma)
         enddo
     enddo
 
