996c18b8d890f328f3f286f47e01536d5b44d108
MiyajiTakenari <100999489+MiyajiTakenari@users.noreply.github.com>
2024-10-09 11:17:10 +0900

    y倍精度、(x-0.4)掛けた

--- glid.f90
+++ glid.f90
@@ -4,13 +4,22 @@ subroutine glid
 
     implicit none
     integer i, j
+    real(8) dx
+    real(8), parameter :: range_x = 1.0d0, range_y = 0.6d0, ramp_x = 0.4d0
+
+    dx = range_x / dble(imax-imin+1)
 
     open(13,file = 'meshfile.txt')
     do j = jmin-3, jmax+2
         do i = imin-3, imax+2
             !物理空間初期分布
-            x(i, j) = -range_x / 2.0d0 + dx * dble(i)
-            y(i, j) = -range_y / 2.0d0 + dy * dble(j)
+            x(i, j) = dx * dble(i)
+            if ( x(i, j)<= ramp_x ) then
+                y(i, j) = (range_y / dble(jmax-jmin+1)) * dble(j)
+            else
+                y(i, j) = (range_y / dble(jmax-jmin+1)) * dble(j) &
+                & + tan(theta) * (1.0d0 - dble(j) / dble(jmax-jmin+1)) * (x(i, j) - ramp_x)
+            endif
             write(13,*) x(i,j), ',',  y(i,j)
         end do
     end do
